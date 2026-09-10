a7550da573baa052b29f37ee063367d51dcb9872
tuestadanilo35-alt <tuestadanilo35@gmail.com>
2026-09-09 19:31:30 -0500

    bot1.js

--- bot1.js
+++ bot1.js
@@ -399,3 +399,8 @@ client.on('interactionCreate', async (interaction) => {
 });
 
 client.login(process.env.TOKEN);
+const http = require('http');
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bot en linea 24/7');
+}).listen(process.env.PORT || 3000);
